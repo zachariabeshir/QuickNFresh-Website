2f47dc06555026364546e0f6c14508f3d7d35acc
zachariabeshir <zachariabeshir2@gmail.com>
2025-06-08 18:12:16 -0700

    Add files via upload

--- backend/model/Company.js
+++ backend/model/Company.js
@@ -1,1 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const companySchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  companyName: {
+    type: String,
+    required: true,
+  },
+  companyEmail: {
+    type: String,
+    required: true,
+  },
+  companyAddress: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  messSubject: {
+    type: String,
+    required: true,
+  },
+  messBody: {
+    type: String,
+    required: true,
+  },
+  dateSubmitted: {
+    type: Date,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Company", companySchema, "Clients"); // Ensure schema name is consistent
