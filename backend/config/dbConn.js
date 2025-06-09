331ce45df55be6ae92c65db783641172cab0c364
zachariabeshir <zachariabeshir2@gmail.com>
2025-06-08 18:02:45 -0700

    Add files via upload

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -1,1 +1,7 @@
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  mongoose.connect(process.env.DATABASE_URI).catch((err) => console.log(err));
+};
+
+module.exports = { connectDB };
